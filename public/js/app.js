f9f87259c23f725a0b08b489b4ccd7413ea28304
Johnson Chong <johnsonhk88@gmail.com>
2020-03-21 15:44:42 +0800

    Add Form  and search button as index page 1. add form input location for weather forecast service

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,25 @@
 console.log("Client side javascript file is loaded!")
 
 
-fetch('http://localhost:3000/weather?address=Japan').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-        
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+
+weatherForm.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    // get form input value
+    const location = search.value
+    fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+
+        })
     })
 })
