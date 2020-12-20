5aff9f75aae7540c1893bec8db870768dc64dce6
lilyleung <lilyleung618618@gmail.com>
2020-12-20 12:16:55 +0800

    Add user login, session saving and weather theme selection function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,8 +2,35 @@ const db = require("../dbmodels");
 const User = db.user;
 const UserSetting = db.usersetting;
 
-exports.normalPage = (req, res) => {
-  res.status(200).send("Normal Page");
+exports.changeTheme = (req, res) => {
+   User.findById(req.userId)
+    .exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+
+      if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+      }
+
+      UserSetting.findOneAndUpdate({username: user.username},{background: req.query.background},{useFindAndModify: false})
+        .exec((err, usersetting) => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+          if (!usersetting) {
+            return res.status(404).send({ message: "UserSetting Not found." });
+          }
+
+          res.status(200).send({
+            username: user.username,
+            background: req.query.background
+          });
+        });
+
+    });
 };
 
 exports.loginedPage = (req, res) => {
